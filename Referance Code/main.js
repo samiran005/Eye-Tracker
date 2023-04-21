0c55e7bb879334f653a3a1b653819309a8a39cae
samiran005 <110296783+samiran005@users.noreply.github.com>
2023-04-21 18:15:30 +0530

    This is First

---
+++ Referance Code/main.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+
+const eyeLeft = document.querySelector('.eyeLeft')
+const eyeRight = document.querySelector('.eyeRight')
+
+container.addEventListener("mousemove", (e)=>{
+
+    let x = e.clientX;
+    let y = e.clientY;
+
+    eyeLeft.style.top = `${y / 10}%`;
+    eyeLeft.style.left = `${(x / 10)- 30}%`;
+
+    eyeRight.style.top = `${y / 10}%`;
+    eyeRight.style.left = `${(x / 10)- 30}%`;
+})
